5f3469f1d48443038efe1e819056dfb26d1c57a1
Tony Tran <ttran@learnvest.com>
2016-12-11 11:10:31 -0500

    Having function output date instead of plain hello world

--- functions/hello/index.js
+++ functions/hello/index.js
@@ -1,5 +1,6 @@
 console.log('starting function')
 exports.handle = function(e, ctx, cb) {
-  console.log('processing event: %j', e)
-  cb(null, { hello: 'world this is a demo' })
+  console.log('processing event: %j', e);
+  var currentDate = new Date().toTimeString();
+  cb(null, { hello: 'hi everyone this is a demo for lv.com ' + currentDate })
 }
