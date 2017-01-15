f6cbb9f0446aba36140da76e25941f987d753c95
Tony Tran <toant713@gmail.com>
2017-01-15 12:03:24 -0500

    Creating lambda function for new Alexa skill

--- functions/hello/index.js
+++
@@ -1,6 +0,0 @@
-console.log('starting function')
-exports.handle = function(e, ctx, cb) {
-  console.log('processing event: %j', e);
-  var currentDate = new Date().toTimeString();
-  cb(null, { hello: 'hi everyone this is a demo for lv.com ' + currentDate })
-}
