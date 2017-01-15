1c74182b7db49722ae68cae57110d511491a5c83
Tony Tran <toant713@gmail.com>
2017-01-15 14:41:49 -0500

    Added initial code to use AlexSkill.js library

--- functions/time-till/index.js
+++ functions/time-till/index.js
@@ -1,5 +1,15 @@
+'use strict';
+
+
 console.log('starting function')
+let AlexaSkill = require('./AlexaSkill');
+
+
+
+console.log('does it exist: ' + AlexaSkill);
+
 exports.handle = function(e, ctx, cb) {
   console.log('processing event: %j', e)
+
   cb(null, { hello: 'world' })
 }
